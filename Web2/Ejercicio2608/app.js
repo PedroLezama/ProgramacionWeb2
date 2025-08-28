b8619d8140e6c22fd3a8861d773fdb106f925e58
PedroLezama <113399945+PedroLezama@users.noreply.github.com>
2025-08-27 18:01:16 -0600

    CarpetaWeb2

---
+++ Web2/Ejercicio2608/app.js
@@ -0,0 +1,13 @@
+let variable1 = parseFloat (prompt("ingresa primer valor"));
+let variable2 = parseFloat (prompt("ingresa segundo valor"));
+let sum = variable1 + variable2;
+let rest = variable1 - variable2;
+let mul =  variable1 * variable2;
+let mod = variable1 % variable2;
+
+console.log("Resultado de suma es: " + sum);
+console.log("Resultado de resta es: " + rest);
+console.log("Resultado de multiplicacion es: " + mul);
+console.log("El modulo es: "+ mod);
+
+alert("Resultado de suma es: " + sum + " de resta es: " + rest + " de multiplicacion es: " + mul + " El modulo es: "+ mod)
